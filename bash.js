3397d595cc5d0f82e9dc876c7358b334c8e60e39
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-09-18 13:03:02 -0400

    'commit 1 for bash node'

--- bash.js
+++ bash.js
@@ -1,12 +1,15 @@
 //Output
 process.stdout.write('prompt > ');
 require('./pwd.js');
+require('./ls.js')
 
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', x => {
   x.toString().trim();
   if (x === 'pwd') {
-    module.exports.pwdFunction;
+    pwdFunction();
+  } else if (x === 'ls') {
+    lsFunction()
   } else {
     process.stdout.write('You typed: ' + x.toString().trim());
     process.stdout.write('\nprompt > ');
