1c3325346bcfb39f365ee01e98e1574617ce83df
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-09-18 13:49:35 -0400

    'ls added'

---
+++ ls.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+//The stdin 'data' event fires after a user types in a line
+const lsFunction = fs.readdir('./','utf8', (err, files) => {
+    if (err) {
+      throw err;
+    } else {
+      process.stdout.write(files.join('\n'));
+      process.stdout.write("\nprompt > ");
+    }
+  })
+
+module.exports = {
+  ls: lsFunction(),
+};
